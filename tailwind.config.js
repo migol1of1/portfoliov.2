ae487fe7701bd7552ee7789672373451ea35f11d
miguel <miguel.sp1106@gmail.com>
2025-09-04 09:55:04 +0800

    tailwind fr cdn to npm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-red": "#ef4444",
+        "primary-blue": "#3b82f6",
+        "primary-yellow": "#eab308",
+        "primary-green": "#22c55e",
+        "red-50": "#fef2f2",
+        "blue-50": "#eff6ff",
+        "yellow-50": "#fefce8",
+        "green-50": "#f0fdf4",
+      },
+    },
   },
+  plugins: [],
 };
